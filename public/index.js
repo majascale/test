e5206404cd38ce401addfdf39c6cd957d5eb82c1
majascale <maja.stojanova@scalefocus.com>
2025-02-07 12:44:50 +0100

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,22 @@
+// NEW
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let mcargs = [
+  {msisdn: "38977772032", sc: "1990", text: "Test on Thursday", service_id: "2724"}
+];
+
+app.post("/mcargs", (req, res) => {
+  //const { name } = req.body;
+  //const id = mcargs.length + 1;
+  //mcargs.push({msisdn, sc, text, service_id });
+  //res.json({ id, name });
+  call another service
+  res.json(mcargs);
+});
+
+app.listen(10000, () => {
+  console.log("Service listening on port 10000");
+});
